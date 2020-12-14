0f78d30f22680988e90c0e140e35fbe06818ffb0
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-15 00:49:40 +0500

    proj init

---
+++ Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowersStore.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "Name isn't be empty")]
+        [StringLength(20, ErrorMessage = "Name can't be more than 20 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Name should not contain special character,numbers or space")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "SecondName isn't be empty")]
+        [StringLength(40, ErrorMessage = "SecondName can't be more than 40 characters")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "SecondName should not contain special character,numbers or space")]
+        public string SecondName { get; set; }
+
+        [Phone]
+        [RegularExpression("((\\+7|7|8)+([0-9]){10})", ErrorMessage = "Please enter valid phone number.")]
+        public int Phone { get; set; }
+
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "The Email field is not a valid.")]
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+
+        public IEnumerable<Basket> Basket;
+
+
+    }
+}
