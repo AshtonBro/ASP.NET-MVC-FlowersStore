4a206c00d7ece24e7a2f8c9914e5d790f0b726c9
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-15 21:29:57 +0500

    DbSet

--- Data/StoreDBContext.cs
+++ Data/StoreDBContext.cs
@@ -1,15 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FlowersStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlowersStore.Data
 {
     public class StoreDBContext : DbContext
     {
-        protected StoreDBContext()
+        public virtual DbSet<User> Users { get; set; }  
+        public virtual DbSet<Basket> Baskets { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductCart> ProductCarts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public StoreDBContext()
         {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //default connection
+            optionsBuilder.UseSqlServer(@"Data Source=ASHTON\ASHTON;Initial Catalog=FlowersStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
         }
 
