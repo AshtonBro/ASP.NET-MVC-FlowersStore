5917a8bd538080cd6d419adbf840e950e3e6a361
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-18 00:39:53 +0500

    try to open dialog

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,7 +40,6 @@ namespace FlowersStore.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        [HttpPost]
         public IActionResult UserLogin()
         {
             var model = new LoginViewModel();
