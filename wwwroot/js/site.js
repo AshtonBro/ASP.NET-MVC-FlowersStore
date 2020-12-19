38e53a8d574ef64cc3b110cbea48ffa6aca1e14a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-19 23:17:27 +0500

    notrh

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -28,10 +28,6 @@
 
 
 // Methods from Max
-// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
 
 
 function ajaxPost(url, formId, callBack) {
