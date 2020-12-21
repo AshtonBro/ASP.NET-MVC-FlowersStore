a2313c15c05e9b6d7d6d84370876d3868c68274a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-21 18:20:21 +0500

    Start

--- ViewModels/StoreViewModel.cs
+++ ViewModels/StoreViewModel.cs
@@ -1,15 +1,12 @@
-﻿using FlowersStore.Data;
-using FlowersStore.Models;
-using System;
+﻿using FlowersStore.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FlowersStore.ViewModels
 {
     public class StoreViewModel
     {
         public string Title { get; set; }
+
         // Filtration
 
         public IEnumerable<Product> Products { get; set; }
