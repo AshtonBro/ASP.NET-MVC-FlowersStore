acba32306bacb83666442d0aaf14252fafdccac6
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-04 19:10:11 +0500

    change quantity value

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -271,3 +271,31 @@ const colorationColorIcon = () => {
 };
 
 colorationColorIcon();
+
+/* Change quantity shopping card */
+
+const changeQuantity = () => {
+    const plusBtnList = document.querySelectorAll('.counter-plus'),
+        minusBtnList = document.querySelectorAll('.counter-minus');
+
+    for (let plusBtn of plusBtnList) {
+        plusBtn.onclick = () => {
+            let currentSpan = plusBtn.previousElementSibling;
+            if (+(currentSpan.innerHTML) < 101)
+            {
+                currentSpan.innerHTML = +(currentSpan.innerHTML) + 1;
+            }
+        }
+    }
+
+    for (let minusBtn of minusBtnList) {
+        minusBtn.onclick = () => {
+            let currentSpan = minusBtn.nextElementSibling;
+            if (+(currentSpan.innerHTML) > 0) {
+                currentSpan.innerHTML = +(currentSpan.innerHTML) - 1;
+            }
+        }
+    }
+};
+
+changeQuantity();
