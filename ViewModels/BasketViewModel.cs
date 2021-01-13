bf03bff6250f2d2d99dc634866c90c6031f16acb
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-13 19:02:11 +0500

    Get user name and arr shopping carts

---
+++ ViewModels/BasketViewModel.cs
@@ -0,0 +1,12 @@
+﻿using FlowersStore.Models;
+using System.Collections.Generic;
+
+namespace FlowersStore.ViewModels
+{
+    public class BasketViewModel
+    {
+        public string UserName { get; set; }
+        public IEnumerable<ShopingCart> ShopingCarts { get; set; }
+
+    }
+}
