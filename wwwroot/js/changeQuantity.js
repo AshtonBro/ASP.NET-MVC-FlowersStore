23144ccccc1408188a0d2be33403c79641bc0d70
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-15 14:39:02 +0500

    quantity change

---
+++ wwwroot/js/changeQuantity.js
@@ -0,0 +1,25 @@
+﻿/* Change quantity shopping card */
+const changeQuantity = () => {
+    const plusBtnList = document.querySelectorAll('.counter-plus'),
+        minusBtnList = document.querySelectorAll('.counter-minus');
+
+    for (let plusBtn of plusBtnList) {
+        plusBtn.onclick = () => {
+            let currentSpan = plusBtn.previousElementSibling;
+            if (+(currentSpan.innerHTML) < 101) {
+                currentSpan.innerHTML = +(currentSpan.innerHTML) + 1;
+            }
+        }
+    }
+
+    for (let minusBtn of minusBtnList) {
+        minusBtn.onclick = () => {
+            let currentSpan = minusBtn.nextElementSibling;
+            if (+(currentSpan.innerHTML) > 1) {
+                currentSpan.innerHTML = +(currentSpan.innerHTML) - 1;
+            }
+        }
+    }
+};
+
+export default changeQuantity;
