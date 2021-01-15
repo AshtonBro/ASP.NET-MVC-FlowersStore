d1b8cfe05da674f0c279d021abef33de1d828b72
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-15 14:38:49 +0500

    slider join

---
+++ wwwroot/js/slickSlider.js
@@ -0,0 +1,47 @@
+﻿// * Slider Slick jQuery path of code
+const slickSlider = () => {
+    $(document).ready(function () {
+        $(".sl").slick({
+            arrows: false,
+            fade: true,
+            cssEase: 'linear',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            speed: 1000,
+            infinite: true
+        });
+
+        $("#singl1").fancybox({
+            helpers: {
+                title: {
+                    type: 'float'
+                }
+            }
+        });
+
+        $("#singl2").fancybox({
+            openEffect: 'elastic',
+            closeEffect: 'elastic',
+
+            helpers: {
+                title: {
+                    type: 'inside'
+                }
+            }
+        });
+
+        $("#singl3").fancybox({
+            openEffect: 'none',
+            closeEffect: 'none',
+            helpers: {
+                title: {
+                    type: 'outside'
+                }
+            }
+        });
+    });
+}
+
+export default slickSlider;
