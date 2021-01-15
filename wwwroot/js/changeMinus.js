8206531466ecd695d77904d04b19fffe36e154c4
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-16 01:06:56 +0500

    func plus quantity and calc sum

---
+++ wwwroot/js/changeMinus.js
@@ -0,0 +1,22 @@
+﻿/*Change quantity and Collect all prices in basket page and count total sum*/
+export function changeMinus(e) {
+    let id = e.id.replace("_M", "");
+    let quantityBasket = id + "_Q";
+    let productPrice = id + "_Pr";
+    let subTotal = id + "_St";
+    let basketTotal = "SumTotalBasket";
+
+    let quantity = document.getElementById(quantityBasket);
+    if (parseInt(quantity.innerText) == 1) {
+        return;
+    }
+
+    quantity.innerText--;
+
+    let subSumTotal = parseInt(document.getElementById(productPrice).innerText) * parseInt(quantity.innerText);
+    document.getElementById(subTotal).innerText = subSumTotal;
+
+    let total = parseInt(document.getElementById(basketTotal).innerText) - parseInt(document.getElementById(productPrice).innerText);
+
+    document.getElementById(basketTotal).innerText = total;
+}
