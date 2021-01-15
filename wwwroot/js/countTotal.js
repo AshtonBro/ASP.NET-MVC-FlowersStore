a82120ce9808e18a47a457b836efed6e653305cc
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-15 14:39:29 +0500

    if page load count total

---
+++ wwwroot/js/countTotal.js
@@ -0,0 +1,14 @@
+﻿/* Collect all prices in basket page and count total sum*/
+const countTotal = () => {
+        let priceAmount = document.querySelectorAll(".basket-price-amount");
+        let amountTotal = document.querySelector(".amount-total");
+
+        let total = 0;
+        for (var i = 0; i < priceAmount.length - 1; i++) {
+            total += parseInt(priceAmount[i].innerText);
+        }
+
+        amountTotal.innerText = total;
+}
+
+export default countTotal;
