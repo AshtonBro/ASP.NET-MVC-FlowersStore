8eb310dcdb9ee170467ec1e15560d13b1d3377c2
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-15 14:40:08 +0500

    box coloration

---
+++ wwwroot/js/colorationColorIcon.js
@@ -0,0 +1,20 @@
+﻿/* Coloration box */
+const colorationColorIcon = () => {
+    const cardColorIcon = document.querySelectorAll('.card-color-icon'),
+        cardColor = document.querySelectorAll('.card-color');
+    for (var i = 0; i < cardColor.length; i++) {
+        switch (cardColor[i].innerHTML) {
+            case 'Зелёный':
+                cardColorIcon[i].style.backgroundColor = 'Green';
+                continue;
+            case 'Желтый':
+                cardColorIcon[i].style.backgroundColor = 'Yellow';
+                continue;
+            case 'Красный':
+                cardColorIcon[i].style.backgroundColor = 'Red';
+                break;
+        }
+    };
+};
+
+export default colorationColorIcon;
