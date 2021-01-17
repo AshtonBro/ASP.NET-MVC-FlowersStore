e5bb038d26f0657be2fffe984df300cb6fd16de7
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-17 21:37:03 +0500

    Eager loading

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,27 @@
-﻿import colorationColorIcon from './colorationColorIcon.js';
+﻿'use strict';
+
+import colorationColorIcon from './colorationColorIcon.js';
 import changeQuantity from './changeQuantity.js';
 import slickSlider from './slickSlider.js';
 import countTotal from './countTotal.js';
 import { changeMinus } from './changeMinus.js';
 import { changePlus } from './changePlus.js';
+import { ajaxPost, ajaxGet, ajaxPostJsonResult, ajaxDefaultPost } from './ajaxHelper.js';
+import { dialogPost, endLoading, hideDialog, openDialog, showDialog, startLoading} from './openDialog.js';
 
-
+slickSlider();
 colorationColorIcon();
 changeQuantity();
-slickSlider();
+window.ajaxPost = ajaxPost;
+window.ajaxGet = ajaxGet;
+window.ajaxPostJsonResult = ajaxPostJsonResult;
+window.ajaxDefaultPost = ajaxDefaultPost;
+window.dialogPost = dialogPost;
+window.endLoading = endLoading;
+window.hideDialog = hideDialog;
+window.openDialog = openDialog;
+window.showDialog = showDialog;
+window.startLoading = startLoading;
 
 if (window.location.pathname == '/Basket/Index') {
     countTotal();
