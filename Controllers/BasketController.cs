a986855288f7fa56c07a8ccde4322d2a3345a97f
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-17 21:36:45 +0500

    EF include()

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FlowersStore.Data;
+using FlowersStore.Helpers;
 using FlowersStore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlowersStore.Controllers
 {
@@ -19,18 +21,11 @@ namespace FlowersStore.Controllers
 
                 var user = db.Users.FirstOrDefault(f => f.Name == "User1");
                 var basket = db.Baskets.FirstOrDefault(f => f.UserId == user.UserId);
-               // model.ShopingCarts = basket.ShopingCarts;
-                model.ShopingCarts = db.ShopingCarts.Where(f => f.Basket.BasketId == basket.BasketId).ToArray();
-
-                foreach (var cart in model.ShopingCarts)
-                {
-                    cart.Product = db.Products.FirstOrDefault(f => f.ProductId == cart.ProductId);
-                    cart.Product.Category = db.Categories.FirstOrDefault(f => f.CategoryId == cart.Product.CategoryId);
-                }
-
+               
+                model.ShopingCarts = db.ShopingCarts.Include(f => f.Product.Category).Where(f => f.Basket.BasketId == basket.BasketId).ToArray();
             }
-
                 return View("~/Views/Basket/Index.cshtml", model);
         }
+
     }
 }
