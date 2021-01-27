b5700801c7fb05cca1761ef2fd08469b15feb6ce
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-27 18:39:57 +0500

    new errror msq

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -53,12 +53,12 @@ namespace FlowersStore.ViewModels
             public string Email { get; set; }
 
             [Required(ErrorMessage = "Password is required")]
-            [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+            [StringLength(255, ErrorMessage = "Password must be between 5 and 255 characters", MinimumLength = 5)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
             [Required(ErrorMessage = "Confirm Password is required")]
-            [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+            [StringLength(255, ErrorMessage = "Password must be between 5 and 255 characters", MinimumLength = 5)]
             [DataType(DataType.Password)]
             [Compare("Password")]
             public string ConfirmPassword { get; set; }
