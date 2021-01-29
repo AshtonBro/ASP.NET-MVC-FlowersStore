f4dd81323f9705403b33255ca6d5a3fbcb9f30a8
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-30 01:13:54 +0500

    abstract class with 5 methods

---
+++ Services/ICrudService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowersStore.Services
+{
+    public interface ICRUDService<T> where T : class
+    {
+        IEnumerable<T> Get(Guid userId);
+        T GetById(Guid id);
+        bool Create(Guid productId, int quantity,Guid userId);
+        bool Update(T model);
+        bool Update(IEnumerable<T> collection);
+        bool Delete(Guid id);
+    }
+}
