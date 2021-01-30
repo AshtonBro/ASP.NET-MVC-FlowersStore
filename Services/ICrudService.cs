9fb632d443f415c17fa2d6f2aa5dba6dcd74884e
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-30 20:09:58 +0500

    modefy Upgrade

--- Services/ICrudService.cs
+++ Services/ICrudService.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FlowersStore.Services
 {
     public interface ICRUDService<T> where T : class
     {
-        IEnumerable<T> Get(Guid userId);
+        IEnumerable<T> Get(Guid id);
         T GetById(Guid id);
-        bool Create(Guid productId, int quantity,Guid userId);
+        bool Create(T model);
         bool Update(T model);
         bool Update(IEnumerable<T> collection);
         bool Delete(Guid id);
