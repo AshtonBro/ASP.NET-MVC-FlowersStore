6e6cfcbc965544a365d95b2fc44db884e777ae6d
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-01-30 20:11:19 +0500

    Realize Methods by CRUD from Interface

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -31,7 +31,38 @@ namespace FlowersStore.Controllers
             var result = _service.Delete(id);
             if (result) return new JsonResult(new { message = "Success deleted item from basket." });
 
-            return new JsonRedirect("Shoping cart not deleted.");
+            return new JsonRedirect("ShopingCart isn't deleted.");
+        }
+
+        public JsonResult AddToBasket(Guid id, int quantity)
+        {
+            if (id != Guid.Empty)
+            {
+                var succes = false;
+                Guid userId = Guid.Parse("63115EDA-142D-40CC-8C39-9CF543D02354");
+                var exisingShopingCart = _service.Get(userId).FirstOrDefault(f => f.ProductId == id);                             
+
+                if (exisingShopingCart == null)
+                {
+                    Basket basket; 
+                    using (StoreDBContext db = new StoreDBContext())
+                    {
+                         basket = db.Baskets.FirstOrDefault(b => b.UserId == (db.Users.FirstOrDefault(f => f.UserId == userId).UserId));
+                    }
+
+                    var newModel = new ShopingCart() { Quantity = quantity, ProductId = id, BasketId = basket.BasketId };
+                     succes = _service.Create(newModel);
+                }
+                else
+                {
+                     exisingShopingCart.Quantity += quantity;
+                     succes = _service.Update(exisingShopingCart);
+                }
+                    
+                if (!succes) return new JsonResult(new { error = "Error while adding product!" });
+                return new JsonResult(new { message = "Thank you! Item added to basket." });
+            }
+            return new JsonResult(new { error = "Error while adding product!" });
         }
 
         public IActionResult Checkout(BasketViewModel model)
