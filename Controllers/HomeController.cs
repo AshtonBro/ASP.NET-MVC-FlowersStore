898a89798e35ee9fa887ba1e497a5bd87993c30f
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-02-05 16:48:15 +0500

    clean usussles namespaces

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FlowersStore.Models;
-using FlowersStore.Data;
 using FlowersStore.ViewModels;
-using System.IO;
 
 namespace FlowersStore.Controllers
 {
