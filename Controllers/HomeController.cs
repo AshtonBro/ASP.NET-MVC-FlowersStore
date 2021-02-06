5eff9e2bffd175ee1c36f0c48070b3d32c131145
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-02-06 23:10:14 +0500

    iAction Result

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace FlowersStore.Controllers
             return View();
         }
 
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
