7601f164b03ef4c3c95b35d8bfbfc295eceba49d
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-02-11 18:32:02 +0500

    Get all products

---
+++ Services/StoreService.cs
@@ -0,0 +1,18 @@
+﻿using FlowersStore.Data;
+using FlowersStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowersStore.Services
+{
+    public class StoreService : IStoreService
+    {
+        public static readonly StoreDBContext _context = new StoreDBContext();
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _context.Products.Include(f => f.Category).ToArray();
+        }
+    }
+}
