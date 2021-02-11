1b678be7451081a563976d7c81093c382b798c51
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-02-11 18:32:28 +0500

    abstract servise for Store

---
+++ Services/IStoreService.cs
@@ -0,0 +1,10 @@
+﻿using FlowersStore.Models;
+using System.Collections.Generic;
+
+namespace FlowersStore.Services
+{
+    public interface IStoreService
+    {
+        IEnumerable<Product> GetProducts();
+    }
+}
