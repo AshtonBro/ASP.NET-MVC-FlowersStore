8fb91ed18725a4a174f1dca87039e07bc9d2cda9
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-02-11 18:30:48 +0500

    Get Users  Basket

---
+++ Services/BasketService.cs
@@ -0,0 +1,18 @@
+﻿using FlowersStore.Data;
+using FlowersStore.Models;
+using System;
+using System.Linq;
+
+namespace FlowersStore.Services
+{
+    public class BasketService : IBasketService
+    {
+        public static readonly StoreDBContext _context = new StoreDBContext();
+        public Basket GetBasket(Guid userId)
+        {
+            if (userId == Guid.Empty) throw new ArgumentException("User id is Empty.");
+            
+            return _context.Baskets.FirstOrDefault(b => b.User.Id == userId);
+        }
+    }
+}
