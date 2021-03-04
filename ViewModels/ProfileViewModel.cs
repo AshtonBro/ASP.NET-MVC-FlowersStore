fd26bd1b564b93456f8cb681569881a6738ebfc7
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-03-04 16:58:57 +0500

    add password prop

--- ViewModels/ProfileViewModel.cs
+++ ViewModels/ProfileViewModel.cs
@@ -29,6 +29,16 @@ namespace FlowersStore.ViewModels
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Not a valid email")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(255, ErrorMessage = "Password must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(255, ErrorMessage = "Password must be between 5 and 255 characters", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
         public DateTime CreateDate { get; set; }
     }
 }
